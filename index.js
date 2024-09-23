4ce7d7135ca9e50d72e3b8670bcdc957c66824c3
Valber <valbermarcelino999@gmail.com>
2024-09-23 15:00:23 -0300

    parte 1

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const offerRoutes = require('./routes/offers');
 
-// Função para ler o arquivo JSON e retornar as ofertas
-function getOffers(callback) {
-    fs.readFile('data.json', 'utf8', (err, data) => {
-        if (err) {
-            callback(err, null);
-            return;
-        }
-        try {
-            const jsonData = JSON.parse(data);
-            callback(null, jsonData.offers);
-        } catch (error) {
-            callback(error, null);
-        }
-    });
-}
+// Middleware para servir arquivos estáticos
+app.use(express.static('views'));
 
-// Rota para listar todas as ofertas
-app.get('/offers', (req, res) => {
-    getOffers((err, offers) => {
-        if (err) {
-            res.status(500).send('Erro ao ler as ofertas');
-        } else {
-            res.json(offers);
-        }
-    });
-});
+// Configurar rota
+app.use('/offers', offerRoutes);
 
-// Iniciando o servidor
+// Inicializar servidor
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
